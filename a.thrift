466b01de786863fa299a69bbcbc4b66ca0a0880c
HanCong03 <121371790@qq.com>
2018-08-24 13:55:10 +0800

    Update a.thrift

--- a.thrift
+++ a.thrift
@@ -1,7 +1,7 @@
 service PoiService {
-    Poi getPoiByPoiId(1:i64 poiId);
-    list<i64> getAllOnlinePoiIdList(1:optional bool isDebug = true, 2:PoiRequest request);
-    list<Poi> getAllOnlinePoi(1:optional bool isDebug = true, 2:PoiRequest request);
+    Poi getPoiByPoiId2(1:i64 poiId);
+    list<i32> getAllOnlinePoiIdList(1:optional bool isDebug = true, 2:PoiRequest request);
+    list<Poi3> getAllOnlinePoi(1:optional bool isDebug = true, 2:optional PoiRequest request, 3:required string base);
 
     /**
     * 注释内容
